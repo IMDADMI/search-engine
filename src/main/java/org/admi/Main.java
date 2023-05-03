89c99cebb08947121cb6f83d7cdb065e5bab6e39
DARK MODE <35972869+IMDADMI@users.noreply.github.com>
2023-05-03 13:47:57 +0100

    Update Main.java

--- src/main/java/org/admi/Main.java
+++ src/main/java/org/admi/Main.java
@@ -13,22 +13,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, ParserConfigurationException, IOException, SAXException {
-//
-//        CenteryTreatment treatment = new CenteryTreatment();
-//        treatment.listTaffsirByCentery();
-//        //show the result of the table
-//        FilterLemma filterLemma = new FilterLemma();
 
-        File dir = new File("src\\lemmesSiecle");
-        if(dir.exists() && dir.isDirectory()) {
-            File files[] = dir.listFiles();
-            int filesLength = files.length;
-            for (int i = 0; i <filesLength; i++) {
-                if(files[i].getName().equals(".gitignore")){
-                    System.out.println("do");
-                }
-            }
-        }
+       CenteryTreatment treatment = new CenteryTreatment();
+       treatment.listTaffsirByCentery();
+       //show the result of the table
+       FilterLemma filterLemma = new FilterLemma();
+
+    
 
     }
 }
